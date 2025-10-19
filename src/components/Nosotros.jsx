772463dbb153bf87ebc0b6ad77732acf5b2b720b
renzo2812 <rmendoza_2812@hotmail.com>
2025-10-19 14:00:34 -0500

    fix: Imagen bien subida

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -8,7 +8,7 @@ const Nosotros = () => {
           <div className='w-full md:w-1/2 flex justify-center'>
             <div className='relative w-[70%] md:w-3/4'>
               <img 
-                src="public/nosotros.webp" 
+                src="/nosotros.webp" 
                 alt="Lubricantes y maquinaria" 
                 className='w-full h-auto shadow-xl rounded-lg'
               />
